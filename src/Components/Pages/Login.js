26354f9c42349097409687ca4c35c80460e3edd6
sushanthpadati <112825522+sushanthpadati@users.noreply.github.com>
2024-03-28 13:29:41 +0530

    Update Login.js

--- src/Components/Pages/Login.js
+++ src/Components/Pages/Login.js
@@ -12,7 +12,7 @@ function Login({ setAuthenticated }) {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/login', {
+      const response = await fetch('http://3.16.11.52:5000/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
